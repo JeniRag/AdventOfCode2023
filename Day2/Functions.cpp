89bf725515214392018129d22107d740d05aea32
JeniRag <sujeni@live.com>
2024-01-19 16:29:19 +0100

    Function description added

--- Day2/Functions.cpp
+++ Day2/Functions.cpp
@@ -3,13 +3,27 @@
 #include <fstream>
 #include <sstream>
 #include <map>
-#include <numeric> //for summing ab vector
+#include <numeric>       //for summing ab vector
 #include <bits/stdc++.h> //to find maximum of vector
 
+/**
+ * @brief Checks which games were possible and returns the sum of the corresponding IDs.
+ *
+ * @param file_path  directory of input path
+ * @param colors the requirement conditions, stating which color must have how many cubes
+ * @return ** int the sum of the ID's
+ */
 int PossibleGames(std::string file_path, std::map<std::string, int> colors) // input a dictionary
 {
     std::fstream inputFile;
     inputFile.open(file_path, std::ios::in);
+
+    // return error if it can't be opened
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Error opening file: " << file_path << std::endl;
+        return 0;
+    }
 
     // To save key and value of dict in seperate list
     std::vector<std::string> key;
@@ -27,7 +41,7 @@ int PossibleGames(std::string file_path, std::map<std::string, int> colors) // i
         std::string line;
         while (getline(inputFile, line))
         {
-            // std::cout<<"--------"<<std::endl;
+
             bool possible = true; // assume every game is possible at first
             auto pos = line.find("Game");
 
@@ -41,12 +55,11 @@ int PossibleGames(std::string file_path, std::map<std::string, int> colors) // i
 
                     std::string intString = line.substr(intPos);
                     ID = std::stoi(intString); // convert ID string to integer
-                    // std::cout << "integer " << ID << std::endl;
                 }
 
-                // std::cout <<"Game "<<ID<<std::endl;
-
+                // iteratre over the colors stated in the requirement
                 std::vector<std::string>::iterator iter = key.begin();
+
                 for (iter; iter < key.end(); iter++)
                 {
 
@@ -56,18 +69,17 @@ int PossibleGames(std::string file_path, std::map<std::string, int> colors) // i
                     {
 
                         // Update the starting position for the next search
-
                         auto noPos = line.find_first_of("0123456789", colorPos - 3); // position of number
                         if (noPos != std::string::npos)
                         {
-                            // std::cout << "adding "<<*iter<< " "<<std::stoi( line.substr(noPos))<<std::endl;
+
                             int amount = std::stoi(line.substr(noPos));
 
                             // if already one color is already above the limit, the game won't work.
                             if (amount > colors.find(*iter)->second)
                             {
                                 possible = false; // game won't work
-                                // std::cout<<"not fulfilled"<<std::endl;
+
                                 break;
                             }
                         }
@@ -75,10 +87,10 @@ int PossibleGames(std::string file_path, std::map<std::string, int> colors) // i
                     }
                 }
 
+                // if the gape is possible, save it's ID in the vector
                 if (possible == true)
                 {
                     possibleGames.push_back(ID);
-                    // std::cout<<"fulfilled"<<std::endl;
                 }
             }
         }
@@ -91,10 +103,26 @@ int PossibleGames(std::string file_path, std::map<std::string, int> colors) // i
     return sumID;
 }
 
+/**
+ * @brief Calculates the power of a set of cube and adds all of them up
+ *
+ * @param file_path directory to input file
+ * @param colors the requirement conditions, stating which color must have how many cubes
+ * @return ** int returns the sum of the powers
+ */
 int FewestNumberOfCubes(std::string file_path, std::vector<std::string> colors) // input a dictionary
 {
     std::fstream inputFile;
     inputFile.open(file_path, std::ios::in);
+
+    // return error if it can't be opened
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Error opening file: " << file_path << std::endl;
+        return 0;
+    }
+
+    // to save the products of each game
     std::vector<int> products;
 
     // Check if file is open
@@ -103,7 +131,8 @@ int FewestNumberOfCubes(std::string file_path, std::vector<std::string> colors) 
         std::string line;
         while (getline(inputFile, line))
         {
-          
+
+            // initiate value
             int product = 1;
 
             auto pos = line.find("Game");
@@ -119,17 +148,15 @@ int FewestNumberOfCubes(std::string file_path, std::vector<std::string> colors) 
                     auto noPos = line.find_first_of("0123456789", colorPos - 3); // position of number
                     if (noPos != std::string::npos)
                     {
-                        // std::cout << "adding "<<*iter<< " "<<std::stoi( line.substr(noPos))<<std::endl;
+
                         int amount = std::stoi(line.substr(noPos));
                         amounts.push_back(amount);
-
-                       
                     }
                     colorPos = line.find(*iter, colorPos + 1);
                 }
 
                 int minimum = *max_element(amounts.begin(), amounts.end());
-                // std::cout<<"Minimum of "<<*iter<< ": "<< minimum <<std::endl;
+
                 product *= minimum;
             }
             products.push_back(product);
